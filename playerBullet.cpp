2acfd5e47ce61e2509123c95d68dc021b14ccac5
takashithiba <k021g1120@g.neec.ac.jp>
2022-11-09 15:57:24 +0900

    スカイドームクラス作成 プレイヤーの移動

--- playerBullet.cpp
+++ playerBullet.cpp
@@ -1,1 +1,50 @@
 #include "playerBullet.h"
+#include"playerBullet.h"
+#include<cassert>
+#include"affine.h"
+
+void playerBullet::Initialize( const Vector3& position, const Vector3& velocity)
+{
+
+	model_->Create();
+	//テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	worldTransform_.Initialize();
+
+	worldTransform_.translation_ = position;
+
+	velocity_ = velocity;
+}
+void playerBullet::Update()
+{
+	if (--deathTimer_ <= 0) {
+		isDead_ = true;
+	}
+
+	worldTransform_.translation_ += velocity_;
+
+	affine::makeMatIdentity(worldTransform_.matWorld_);
+	affine::makeMatRot(worldTransform_.matWorld_, worldTransform_.rotation_);
+	affine::makeMatTrans(worldTransform_.matWorld_, worldTransform_.translation_);
+	worldTransform_.TransferMatrix();
+}
+void playerBullet::Draw(const ViewProjection& viewProjection)
+{
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
+void playerBullet::OnCollision()
+{
+	isDead_ = true;
+}
+Vector3 playerBullet::GetworldPosition()
+{
+	//ワールド座標を入れる変数
+	Vector3 worldpos;
+	//ワールド行列の平行移動成分を取得（ワールド座標）
+	worldpos.x = worldTransform_.translation_.x;
+	worldpos.y = worldTransform_.translation_.y;
+	worldpos.z = worldTransform_.translation_.z;
+
+	return worldpos;
+}
